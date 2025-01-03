ac36a512b9bda41baf6ce2048348fb46568bedb7
matheus mozart <mmsnborges@gmail.com>
2025-01-03 12:33:11 -0300

    members and teste

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { EnvConfig } from './config';
+import { corsOptions } from './config/cors';
+import { appConfig } from './config/app.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule, { cors: corsOptions });
+  appConfig(app)
 
   const config = new DocumentBuilder()
-    .setTitle('Hello world')
-    .setDescription('The cats API description')
+    .setTitle('Back-end fusion')
+    .setDescription('Fusion do projeto colaborativo do fusion.')
     .setVersion('1.0')
     .addTag('fusion')
+    .addTag('member')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(3000);
+  await app.listen(EnvConfig.PORT);
+  console.log(`APP STARTED ON PORT: ${EnvConfig.PORT}`)
 }
 bootstrap();
